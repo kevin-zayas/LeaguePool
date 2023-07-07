87a71cb37ddc0607a75e8a5b07f5bf08d9d38fba
Kevin Zayas <kzayas@mit.edu>
2023-07-06 21:42:44 -0500

    move close client to API call to prevent error

--- league_pool_app.py
+++ league_pool_app.py
@@ -5,11 +5,12 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://localhost:27017/')
-DB = client['LeaguePool']
 
 @app.route('/champion-pool', methods=['GET'])
 def get_champion_pool():
+    client = MongoClient('mongodb://localhost:27017/')
+    DB = client['LeaguePool']
+
     current_champions = request.args.get('current_champions')
     if current_champions:
         current_champions = current_champions.split(',')
@@ -23,13 +24,10 @@ def get_champion_pool():
         exclude_champions = []
 
     champion_pool = calc_champion_pool(DB, current_champions,exclude_champions)
+    client.close()
     return {'champion_pool': champion_pool}     # Return the champion pool as a JSON response
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    # Close the MongoDB connection
-    client.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')  # Listen on all network interfaces
